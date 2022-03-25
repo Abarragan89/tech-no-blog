f5ee9e74b102816f19b46812d4c9c035d42173e7
Anthony Barragan <anthony.bar.89@gmail.com>
2022-03-24 22:05:04 -0700

    made connection with server

---
+++ routes.js
@@ -0,0 +1,9 @@
+// test route
+const router = require('express').Router();
+
+
+router.get('/', (req, res) => {
+    res.json('hello world')
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,18 @@
+// Set up express
+const express = require('express');
+const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const routes = require('./routes')
+
+// express middleware for JSON decoding
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(routes);
+
+// turn on server and DB connection
+sequelize.sync({ force: false })
+    .then(() => {
+        app.listen(PORT, () => console.log('Now listening'))
+    })
