3b02af7b9bcf61c293bc4bdc356d57f48f5c3e59
Anthony Barragan <anthony.bar.89@gmail.com>
2022-03-28 22:19:25 -0700

    added edit button to dashboard

--- controllers/api/comments-routes.js
+++ controllers/api/comments-routes.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 router.post('/:blog_id/:user_id', (req, res) => {
     Comment.create({
         comment_text: req.body.comment_text,
-        user_id: req.params.user_id,
+        user_id: req.session.user_id,
         blog_id: req.params.blog_id
     })
     .then(dbCommentData => {
